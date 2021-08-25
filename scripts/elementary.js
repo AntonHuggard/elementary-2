a767673e5c5288c79dc0f412a6a2ec6cf2131707
AntonHuggard <56659693+AntonHuggard@users.noreply.github.com>
2021-08-25 21:24:25 +1200

    Patched accordion bug

--- scripts/elementary.js
+++ scripts/elementary.js
@@ -21,19 +21,9 @@ function getHeight() {
 }
 
 
-function show_accordian() {
-    var acc = document.getElementsByClassName("accordion");
-    for (var i = 0; i < acc.length; i++) {
-        acc[search_type].classList.toggle("active");
-        // toggle search type panel 
-        var panel = document.getElementsByClassName("panel");
-        if (panel[0].style.display === "block") {
-            panel[0].style.display = "none";
-        } else {
-            panel[0].style.display = "block";
-        }
-
-    }
+function show_accordian(object) {
+    object.classList.toggle("active");
+    document.getElementById("drpdwn-menu").classList.toggle("begone");
 }
 
 var search_type = 0;
@@ -42,34 +32,29 @@ function set_search(n, mobile_view) {
 
     // from here ...
 
-    document.getElementById("acc_dt").style.display = "none";
-    document.getElementById("acc_EN").style.display = "none";
-    document.getElementById("acc_MP").style.display = "none";
-    document.getElementById("acc_BP").style.display = "none";
-    document.getElementById("acc_ds").style.display = "none";
     document.getElementById("electronegativity_div").style.display = "none";
     document.getElementById("melting_pt_div").style.display = "none";
     document.getElementById("boiling_pt_div").style.display = "none";
     document.getElementById("discovery_div").style.display = "none";
 
+    const acc = document.getElementById("accordion_btn");
+
     if (mobile_view) {
-        var acc = document.getElementsByClassName("accordion");
-        acc[search_type].classList.toggle("active");
         switch (n) {
             case 0:
-                document.getElementById("acc_dt").classList.toggle("active");
+                acc.innerText = "default";
                 break;
             case 1:
-                document.getElementById("acc_EN").classList.toggle("active");
+                acc.innerText = "electronegativity";
                 break;
             case 2:
-                document.getElementById("acc_MP").classList.toggle("active");
+                acc.innerText = "melting point";
                 break;
             case 3:
-                document.getElementById("acc_BP").classList.toggle("active");
+                acc.innerText = "boiling point";
                 break;
             case 4:
-                document.getElementById("acc_ds").classList.toggle("active");
+                acc.innerText = "discovery";
                 break;
         }
     }
@@ -79,69 +64,61 @@ function set_search(n, mobile_view) {
 
     switch (n) {
         case 0:
-            document.getElementById("acc_dt").style.display = "block";
             document.getElementById("element_search_container").style.display = "grid";
             reset_opacity();
+            acc.innerText = "default";
             search_type = 0;
             break;
         case 1:
             if (search_type != n) {
-                document.getElementById("acc_EN").style.display = "block";
                 document.getElementById("electronegativity_div").style.display = "block";
                 document.getElementById("element_search_container").style.display = "none";
                 en_slider_function();
                 search_type = 1;
             } else {
-                document.getElementById("acc_dt").style.display = "block";
-                document.getElementById("acc_dt").classList.toggle("active");
                 document.getElementById("element_search_container").style.display = "grid";
                 reset_opacity();
+                acc.innerText = "default";
                 search_type = 0;
             }
             break;
         case 2:
             if (search_type != n) {
                 search_type = 2;
-                document.getElementById("acc_MP").style.display = "block";
                 document.getElementById("melting_pt_div").style.display = "block";
                 document.getElementById("element_search_container").style.display = "none";
                 mp_slider_function();
             } else {
-                document.getElementById("acc_dt").style.display = "block";
                 document.getElementById("element_search_container").style.display = "grid";
                 reset_opacity();
                 search_type = 0;
+                acc.innerText = "default";
             }
             break;
         case 3:
             if (search_type != n) {
                 search_type = 3;
-                document.getElementById("acc_BP").style.display = "block";
-                document.getElementById("acc_BP").classList.toggle("active");
                 document.getElementById("boiling_pt_div").style.display = "block";
                 document.getElementById("element_search_container").style.display = "none";
                 bp_slider_function();
             } else {
-                document.getElementById("acc_dt").style.display = "block";
-                document.getElementById("acc_dt").classList.toggle("active");
                 document.getElementById("element_search_container").style.display = "grid";
                 reset_opacity();
                 search_type = 0;
+                acc.innerText = "default";
             }
             break;
         case 4:
             if (search_type != n) {
                 search_type = 4;
-                document.getElementById("acc_ds").style.display = "block";
                 document.getElementById("discovery_div").style.display = "block";
                 document.getElementById("element_search_container").style.display = "none";
                 ds_slider_function();
             } else {
-                document.getElementById("acc_dt").style.display = "block";
-                document.getElementById("acc_dt").classList.toggle("active");
                 document.getElementById("element_search_container").style.display = "grid";
                 reset_opacity();
                 search_type = 0;
+                acc.innerText = "default";
             }
             break;
     }
