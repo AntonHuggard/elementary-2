60cfbb7ea12e2511b49712299e946ce011d50cb5
AntonHuggard <antonhuggard@gmail.com>
2023-02-23 00:21:13 +1300

    Updates

---
+++ precache-manifest.a4d343def77e23c5c91446509bc78ea2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ff5db373a3a3fe002748",
+    "url": "/static/css/main.8e91d95d.chunk.css"
+  },
+  {
+    "revision": "ff5db373a3a3fe002748",
+    "url": "/static/js/main.ff5db373.chunk.js"
+  },
+  {
+    "revision": "f09d6da400d0c14a7f1d",
+    "url": "/static/js/1.f09d6da4.chunk.js"
+  },
+  {
+    "revision": "1b8b8b4b84561c07d882",
+    "url": "/static/js/2.1b8b8b4b.chunk.js"
+  },
+  {
+    "revision": "0e85d9ea22f618c5f874",
+    "url": "/static/js/runtime~main.0e85d9ea.js"
+  },
+  {
+    "revision": "99d64bef7379c853f19883e296566a08",
+    "url": "/static/media/background-2.99d64bef.jpg"
+  },
+  {
+    "revision": "dbde3e8aa54d891d4c8eeec54c777fad",
+    "url": "/index.html"
+  }
+];
