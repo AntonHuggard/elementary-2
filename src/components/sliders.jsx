a38ac3d7d96969052c9710526c786b6ce0cb147b
AntonHuggard <antonhuggard@gmail.com>
2023-04-19 21:37:26 +1000

    + icons for the slider menu

--- src/components/sliders.jsx
+++ src/components/sliders.jsx
@@ -1,13 +1,25 @@
 import React, { Component } from 'react'
+import electron from './imgs/btn-icon-electron.png';
+import cloud from './imgs/btn-icon-cloud.png';
+import drop from './imgs/btn-icon-drop.png';
 
 class SliderMenu extends Component {
 
     render() {
         return ( 
             <div id='slider-menu'>
-                <button onClick={ () => {this.props.onSelectQuery("en-slider")} }>electroegativity</button>
-                <button onClick={ () => {this.props.onSelectQuery("mp-slider")} }>melting point</button>
-                <button onClick={ () => {this.props.onSelectQuery("bp-slider")} }>boiling point</button>
+                <button onClick={ () => {this.props.onSelectQuery("en-slider")} }>
+                    <img src={electron} alt="electronegativity symbol" className='slider-menu-icon'/>
+                    <span className='slider-menu-txt'>electroegativity</span>
+                    </button>
+                <button onClick={ () => {this.props.onSelectQuery("mp-slider")} }>
+                    <img src={drop} alt="mp symbol" className='slider-menu-icon'/>
+                    <span className='slider-menu-txt'>melting point</span>
+                    </button>
+                <button onClick={ () => {this.props.onSelectQuery("bp-slider")} }>
+                    <img src={cloud} alt="bp symbol" className='slider-menu-icon'/>
+                    <span className='slider-menu-txt'>boiling point</span>
+                    </button>
                 <button onClick={ () => {this.props.onSelectQuery("discovery")} }>discovery</button>
             </div>
         );
