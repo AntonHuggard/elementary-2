5090e988f14e4111f3bd66f6b69b4aac51734eb1
AntonHuggard <antonhuggard@gmail.com>
2023-10-02 12:20:37 +1300

    manually specifying all ion grid positions

--- src/components/ion.jsx
+++ src/components/ion.jsx
@@ -7,9 +7,10 @@ class Ion extends React.Component {
         const chrg = this.props.ion.charge;
         const chargeColumn = chrg > 0? chrg + 3: Math.abs(chrg) ;
         const ionClasses = "ion-box ion-col-"+chargeColumn
+        const ionID = 'ion-'+this.props.ion.id
 
         return (
-                <div className={ionClasses} >
+                <div className={ionClasses} id={ionID}>
                     <p dangerouslySetInnerHTML={{ __html: this.props.ion.html }}></p>
                     <p className="ion-name">{this.props.ion.name}</p>
                 </div>
