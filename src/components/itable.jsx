c14731d24ccdc40ee0ffb1f6f09ec2df2aa3d229
AntonHuggard <antonhuggard@gmail.com>
2023-10-02 11:58:12 +1300

    init mobile view styling

--- src/components/itable.jsx
+++ src/components/itable.jsx
@@ -9,12 +9,12 @@ class IonTable extends Component {
                 <div id="ion-table">
                     <label id="anion-header">Anions</label>
                     <label id="cation-header">Cations</label>
-                    <label className='ion-col-1'>-1</label>
-                    <label className='ion-col-2'>-2</label>
-                    <label className='ion-col-3'>-3</label>
-                    <label className='ion-col-4'>+1</label>
-                    <label className='ion-col-5'>+2</label>
-                    <label className='ion-col-6'>+3</label>
+                    <label className='ion-col-1 anion-chrg-hdr'>-1</label>
+                    <label className='ion-col-2 anion-chrg-hdr'>-2</label>
+                    <label className='ion-col-3 anion-chrg-hdr'>-3</label>
+                    <label className='ion-col-4 cation-chrg-hdr'>+1</label>
+                    <label className='ion-col-5 cation-chrg-hdr'>+2</label>
+                    <label className='ion-col-6 cation-chrg-hdr'>+3</label>
                     {this.props.ions.map(i => (
                         <Ion key={i.id} ion={i}/>
                         ))
