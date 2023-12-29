7a4229a1c74e0c7182592bcbd0978020e6e3c3d1
AntonHuggard <antonhuggard@gmail.com>
2023-12-29 22:55:16 +1300

    Beefed up H details pg

--- src/pages/Hydrogen.js
+++ src/pages/Hydrogen.js
@@ -3,6 +3,7 @@ import Header from '../components/Header';
 import Sidenav from '../components/SideNav';
 import atoms from '../components/atoms.json';
 import {Helmet} from "react-helmet";
+import hindenburg from '../imgs/Hindenburg.jpg';
 
 class Hydrogen extends Component {
 
@@ -40,7 +41,7 @@ class Hydrogen extends Component {
 
                 <div id='content'>
                   <div className='atom-details'>
-                    <h1>{this.state.atom.name}</h1>  
+                    <h1>{this.state.atom.name} ({this.state.atom.symbol})</h1>  
                     <div id='atom-data'>
                       <p>Atomic number: {this.state.atom.id}</p>
                       <p>Atomic mass: {this.state.atom.atomic_mass}</p>   
@@ -53,6 +54,22 @@ class Hydrogen extends Component {
                             <div id="nucleus"></div>
                             <div id="s-orbital-path"></div>
                         </div>
+                    </div>
+                    <div id="atom-description" >
+                      <hr className='horizontal-line' />
+                      <h3>Description & Facts</h3>
+                      <p dangerouslySetInnerHTML={{ __html: this.state.atom.description }} />
+                    </div>
+                    <img id="historic-img" src={hindenburg} alt='Hindenburg disaster' />
+                    <div id="atom-etymology">
+                      <hr className='horizontal-line' />
+                      <h3>Etymology</h3>
+                      <p dangerouslySetInnerHTML={{ __html: this.state.atom.etymology }} />
+                    </div>
+                    <div id="atom-discovery">
+                      <hr className='horizontal-line' />
+                      <h3>Discovery</h3>
+                      <p dangerouslySetInnerHTML={{ __html: this.state.atom.discovery_details }} />
                     </div>
                   </div>
                 </div>
