31e0c6e99d446995a539a6ebb7032969d8c90c64
AntonHuggard <antonhuggard@gmail.com>
2024-01-25 23:18:17 +1300

    Au 5d electrons

--- src/pages/elements/Gold.js
+++ src/pages/elements/Gold.js
@@ -55,14 +55,23 @@ class Gold extends Component {
                       <p>Atomic number: {this.state.atom.id}</p>
                       <p>Atomic mass: {this.state.atom.atomic_mass}</p>   
                       <p>Row/Period N<sup>o</sup> : {this.state.atom.period}</p>   
-                      <p>Group N<sup>o</sup> : {this.state.atom.group}</p>   
+                      <p>Group N<sup>o</sup> : {this.state.atom.group}</p>  
+                      <p>Oxidation states: -3, -2, -1, +1, +2, +3, +5</p> 
                     </div>
                     <div id="atom-container">
                         <div id="atom">
-                            <div id="electron-1" class="electron"></div>
-                            <div id="electron-2" class="electron"></div>
-                            <div id="electron-3" class="electron"></div>
-                            <div id="nucleus" className='alkali-metal-nucleus'></div>
+                            <div id="d-elctr-1" class="electron d-electr"></div>
+                            <div id="d-elctr-2" class="electron d-electr"></div>
+                            <div id="d-elctr-3" class="electron d-electr"></div>
+                            <div id="d-elctr-4" class="electron d-electr"></div>
+                            <div id="d-elctr-5" class="electron d-electr"></div>
+                            <div id="d-elctr-6" class="electron d-electr"></div>
+                            <div id="d-elctr-7" class="electron d-electr"></div>
+                            <div id="d-elctr-8" class="electron d-electr"></div>
+                            <div id="d-elctr-9" class="electron d-electr"></div>
+                            <div id="d-elctr-10" class="electron d-electr"></div>
+
+                            <div id="nucleus" className='transition-metal-nucleus'></div>
                             <div id="s-orbital-path"></div>
                             <div id="s2-orbital-path"></div>
                             <label id='ionic-charge' className='alkali-metal-txt ghost'>+</label>
