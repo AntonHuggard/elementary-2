2a8b8b77d662394889955c2d491b477ecc4d7648
AntonHuggard <antonhuggard@gmail.com>
2024-02-01 22:30:40 +1300

    + links for every element page

--- src/App.js
+++ src/App.js
@@ -142,16 +142,121 @@ class App extends Component {
                 <Route path="/hydrogen" element={<Hydrogen />} />
                 <Route path="/helium" element={<Helium />} />
                 <Route path="/lithium" element={<Lithium />} />
+                <Route path="/beryllium" element={<Beryllium />} />
+                <Route path="/boron" element={<Boron />} />
                 <Route path="/carbon" element={<Carbon />} />
+                <Route path="/nitrogen" element={<Nitrogen />} />
+                <Route path="/oxygen" element={<Oxygen />} />
                 <Route path="/fluorine" element={<Fluorine />} />
                 <Route path="/neon" element={<Neon />} />
-                <Route path="/oxygen" element={<Oxygen />} />
-                <Route path="/gold" element={<Gold />} />
-                <Route path="/iron" element={<Iron />} />
-                <Route path="/actinium" element={<Actinium />} />
+                <Route path="/sodium" element={<Sodium />} />
+                <Route path="/magnesium" element={<Magnesium />} />
                 <Route path="/aluminium" element={<Aluminium />} />
-                <Route path="/americium" element={<Americium />} />
+                <Route path="/silicon" element={<Silicon />} />
+                <Route path="/phosphorus" element={<Phosphorus />} />
+                <Route path="/sulphur" element={<Sulphur />} />
+                <Route path="/chlorine" element={<Chlorine />} />
+                <Route path="/argon" element={<Argon />} />
+                <Route path="/potassium" element={<Potassium />} />
+                <Route path="/calcium" element={<Calcium />} />
+                <Route path="/scandium" element={<Scandium />} />
+                <Route path="/titanium" element={<Titanium />} />
+                <Route path="/vanadium" element={<Vanadium />} />
+                <Route path="/chromium" element={<Chromium />} />
+                <Route path="/manganese" element={<Manganese />} />
+                <Route path="/iron" element={<Iron />} />
+                <Route path="/cobalt" element={<Cobalt />} />
+                <Route path="/nickel" element={<Nickel />} />
+                <Route path="/copper" element={<Copper />} />
+                <Route path="/zinc" element={<Zinc />} />
+                <Route path="/gallium" element={<Gallium />} />
+                <Route path="/germanium" element={<Germanium />} />
+                <Route path="/arsenic" element={<Arsenic />} />
+                <Route path="/selenium" element={<Selenium />} />
+                <Route path="/bromine" element={<Bromine />} />
+                <Route path="/krypton" element={<Krypton />} />
+                <Route path="/rubidium" element={<Rubidium />} />
+                <Route path="/strontium" element={<Strontium />} />
+                <Route path="/yttrium" element={<Yttrium />} />
+                <Route path="/zirconium" element={<Zirconium />} />
+                <Route path="/niobium" element={<Niobium />} />
+                <Route path="/molybdenum" element={<Molybdenum />} />
+                <Route path="/technetium" element={<Technetium />} />
+                <Route path="/ruthenium" element={<Ruthenium />} />
+                <Route path="/rhodium" element={<Rhodium />} />
+                <Route path="/palladium" element={<Palladium />} />
+                <Route path="/silver" element={<Silver />} />
+                <Route path="/cadmium" element={<Cadmium />} />
+                <Route path="/indium" element={<Indium />} />
+                <Route path="/tin" element={<Tin />} />
                 <Route path="/antimony" element={<Antimony />} />
+                <Route path="/tellurium" element={<Tellurium />} />
+                <Route path="/iodine" element={<Iodine />} />
+                <Route path="/xenon" element={<Xenon />} />
+                <Route path="/caesium" element={<Caesium />} />
+                <Route path="/barium" element={<Barium />} />
+                <Route path="/lanthanum" element={<Lanthanum />} />
+                <Route path="/cerium" element={<Cerium />} />
+                <Route path="/praseodymium" element={<Praseodymium />} />
+                <Route path="/neodymium" element={<Neodymium />} />
+                <Route path="/promethium" element={<Promethium />} />
+                <Route path="/samarium" element={<Samarium />} />
+                <Route path="/europium" element={<Europium />} />
+                <Route path="/gadolinium" element={<Gadolinium />} />
+                <Route path="/terbium" element={<Terbium />} />
+                <Route path="/dysprosium" element={<Dysprosium />} />
+                <Route path="/holmium" element={<Holmium />} />
+                <Route path="/erbium" element={<Erbium />} />
+                <Route path="/thulium" element={<Thulium />} />
+                <Route path="/ytterbium" element={<Ytterbium />} />
+                <Route path="/lutetium" element={<Lutetium />} />
+                <Route path="/hafnium" element={<Hafnium />} />
+                <Route path="/tantalum" element={<Tantalum />} />
+                <Route path="/tungsten" element={<Tungsten />} />
+                <Route path="/rhenium" element={<Rhenium />} />
+                <Route path="/osmium" element={<Osmium />} />
+                <Route path="/iridium" element={<Iridium />} />
+                <Route path="/platinum" element={<Platinum />} />
+                <Route path="/gold" element={<Gold />} />
+                <Route path="/mercury" element={<Mercury />} />
+                <Route path="/thallium" element={<Thallium />} />
+                <Route path="/lead" element={<Lead />} />
+                <Route path="/bismuth" element={<Bismuth />} />
+                <Route path="/polonium" element={<Polonium />} />
+                <Route path="/astatine" element={<Astatine />} />
+                <Route path="/radon" element={<Radon />} />
+                <Route path="/francium" element={<Francium />} />
+                <Route path="/radium" element={<Radium />} />
+                <Route path="/actinium" element={<Actinium />} />
+                <Route path="/thorium" element={<Thorium />} />
+                <Route path="/protactinium" element={<Protactinium />} />
+                <Route path="/uranium" element={<Uranium />} />
+                <Route path="/neptunium" element={<Neptunium />} />
+                <Route path="/plutonium" element={<Plutonium />} />
+                <Route path="/americium" element={<Americium />} />
+                <Route path="/curium" element={<Curium />} />
+                <Route path="/berkelium" element={<Berkelium />} />
+                <Route path="/californium" element={<Californium />} />
+                <Route path="/einsteinium" element={<Einsteinium />} />
+                <Route path="/fermium" element={<Fermium />} />
+                <Route path="/mendelevium" element={<Mendelevium />} />
+                <Route path="/nobelium" element={<Nobelium />} />
+                <Route path="/lawrencium" element={<Lawrencium />} />
+                <Route path="/rutherfordium" element={<Rutherfordium />} />
+                <Route path="/dubnium" element={<Dubnium />} />
+                <Route path="/seaborgium" element={<Seaborgium />} />
+                <Route path="/bohrium" element={<Bohrium />} />
+                <Route path="/hassium" element={<Hassium />} />
+                <Route path="/meitnerium" element={<Meitnerium />} />
+                <Route path="/darmstadtium" element={<Darmstadtium />} />
+                <Route path="/roentgenium" element={<Roentgenium />} />
+                <Route path="/copernicium" element={<Copernicium />} />
+                <Route path="/nihonium" element={<Nihonium />} />
+                <Route path="/flerovium" element={<Flerovium />} />
+                <Route path="/moscovium" element={<Moscovium />} />
+                <Route path="/livermorium" element={<Livermorium />} />
+                <Route path="/tennessine" element={<Tennessine />} />
+                <Route path="/oganesson" element={<Oganesson />} />
                 <Route path="/ions" element={<Ions />} />
                 <Route path="*" element={<NotFound />} />
               </Routes>
